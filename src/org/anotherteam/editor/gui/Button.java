f54c83760a5a460f32188be46b33eba92a4aa580
Kirill <kirillirikvideo@gmail.com>
2021-10-14 09:15:46 +0300

    feat: button change color if clicked

--- src/org/anotherteam/editor/gui/Button.java
+++ src/org/anotherteam/editor/gui/Button.java
@@ -10,12 +10,20 @@ import org.lwjgl.glfw.GLFW;
 
 public class Button extends Label {
 
+    private float releaseTime;
+    private float timeToRelease;
     private boolean clicked;
     private Runnable runnable;
 
     public Button(String text, @NotNull Vector2f offset, int height) {
         super(text, offset, height);
+        releaseTime = 1.0f;
+        timeToRelease = 0.0f;
         color = new Color(0, 100, 100, 255);
+    }
+
+    public void setReleaseTime(float releaseTime) {
+        this.releaseTime = releaseTime;
     }
 
     public void setRunnable(Runnable runnable) {
@@ -24,12 +32,18 @@ public class Button extends Label {
 
     @Override
     public void update(float dt) {
+        if (timeToRelease > 0.0f) {
+            timeToRelease -=dt;
+            return;
+        }
         clicked = false;
+        color.r = 0;
         if (!mouseOnWidget()) return;
         if (!Input.isButtonDown(GLFW.GLFW_MOUSE_BUTTON_LEFT)) return;
+        timeToRelease = releaseTime;
         clicked = true;
         if (runnable != null) runnable.run();
-        color.r += 1;
+        color.r = 255;
     }
 
     public boolean mouseOnWidget() {
@@ -40,5 +54,9 @@ public class Button extends Label {
         if (mouseY < pos.y + offset.y || mouseY > pos.y + offset.y + height) return false;
         Editor.setLogText("Mouse!");
         return true;
+    }
+
+    public boolean isClicked() {
+        return clicked;
     }
 }
