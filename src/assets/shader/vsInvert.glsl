5d9441fe7e750506c71b95072b4e19e8b35d7535
Kirill <kirillirikvideo@gmail.com>
2022-08-09 15:10:23 +0300

    feat: clear trash code

--- src/assets/shader/vsInvert.glsl
+++ src/assets/shader/vsInvert.glsl
@@ -14,17 +14,3 @@ void main() {
    tex_positions = vertex_pos;
    gl_Position = projection * view * vec4(vertex_pos, 0.0, 1.0);
 }
-
-//#version 460 core
-//
-//in vec2 vertex_pos;
-//in vec2 in_tex_coord;
-//
-//uniform mat4 projection;
-//uniform mat4 view;
-//
-//out vec2 tex_coord;
-//
-//void main() {
-//   tex_coord = in_tex_coord;
-//   gl_Position = projection * view * vec4(vertex_pos.xy, 0.0, 1.0);
